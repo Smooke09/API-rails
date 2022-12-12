210eaa14e7dbe82ff47630edc1c8d66d980bea30
Smooke09 <pedrolucas1014zz@gmail.com>
2022-12-12 14:47:45 -0300

    feat: insert devise validation

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem "bootsnap", require: false
 # gem "rack-cors"
 
 
+gem 'devise', '~> 4.2'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, defaults: { format: :json }, path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' }, controllers: { sessions: 'sessions' }
+
+  resource :user, only: [:show, :update]
+
   resources :tasks
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
